9c7b44d7f6c5a925d6c24ee967065bc938fd9572
Sarah Hassan Ahmed <sarah27h88@yahoo.com>
2021-04-25 23:53:15 +0200

    feat: add scrollspy

--- Js/app.js
+++ Js/app.js
@@ -1,52 +1,49 @@
 /************ navbar Variables **************/
 let navbar = document.querySelector('.navbar-menu'),
-    navList = document.getElementById('navbar-list');
+  navList = document.getElementById('navbar-list');
 
 /************ Page Global Variables **************/
 const htmlForm = document.getElementById('htmlForm'),
-      frontEndForm = document.getElementById('frontEndForm'),
-      cssForm = document.getElementById('cssForm'),
-      jsForm = document.getElementById('jsForm'),
-      gitForm = document.getElementById('gitForm'),
-      githubForm = document.getElementById('githubForm'),
-      arabicProject = document.getElementById('arabicProject'),
-      englishProject = document.getElementById('englishProject'),
-      readForm = document.getElementById('readForm'),
-      practiceForm = document.getElementById('practiceForm'),
-      quizForm = document.getElementById('quizForm'),
-      cheatSheetForm = document.getElementById('cheatSheetForm'),
-      scrollUpBtn = document.getElementById('upBtn');
-
-
+  frontEndForm = document.getElementById('frontEndForm'),
+  cssForm = document.getElementById('cssForm'),
+  jsForm = document.getElementById('jsForm'),
+  gitForm = document.getElementById('gitForm'),
+  githubForm = document.getElementById('githubForm'),
+  arabicProject = document.getElementById('arabicProject'),
+  englishProject = document.getElementById('englishProject'),
+  readForm = document.getElementById('readForm'),
+  practiceForm = document.getElementById('practiceForm'),
+  quizForm = document.getElementById('quizForm'),
+  cheatSheetForm = document.getElementById('cheatSheetForm'),
+  scrollUpBtn = document.getElementById('upBtn');
 
 /**************************** 
        Global Functions 
  ****************************/
 
-//1- Function to open form 
-const openForm = (id)=> {
-  id.classList.add("open");
-  window.scrollTo(0,0);
+//1- Function to open form
+const openForm = (id) => {
+  id.classList.add('open');
+  window.scrollTo(0, 0);
   return;
-}
+};
 
-//2- Function to close form  
-const closeForm = (id)=> {
-  id.classList.remove("open");
+//2- Function to close form
+const closeForm = (id) => {
+  id.classList.remove('open');
   return;
-}
+};
 
 //3- Function to change navbar color when scroll down [[[ok]]]
-const changeBackgroud = ()=> {
-  if(window.scrollY >= 90) {
+const changeBackgroud = () => {
+  if (window.scrollY >= 90) {
     navbar.classList.add('scroll');
     scrollUpBtn.classList.add('show');
-  }else{
+  } else {
     document.querySelector('.navbar-menu').classList.remove('scroll');
     scrollUpBtn.classList.remove('show');
-
   }
-}
+};
 
 //4- Function to scroll top whith smooth effect comes from css
 const scrollToTop = () => {
@@ -54,26 +51,38 @@ const scrollToTop = () => {
   window.scrollTo({
     top: 0,
     left: 0,
-    behavior: 'smooth'
+    behavior: 'smooth',
   });
-}
+};
 
 //5- Function to handle open & close side menu
-const openIconHandler = ()=> {
+const openIconHandler = () => {
   document.getElementById('1').classList.toggle('line1');
   document.getElementById('2').classList.toggle('line2');
   document.getElementById('3').classList.toggle('line3');
   document.getElementById('sideMenu').classList.toggle('open');
   document.getElementById('cover').classList.toggle('openCover');
-}
+};
 
 /******************************
     Define EVENT LISTENERS  
 ******************************/
- 
-// Change Navbar color when scroll down
-window.addEventListener('scroll', ()=> {
-  changeBackgroud();
-}
-);
 
+// Change Navbar color when scroll down
+window.addEventListener('scroll', () => {
+  changeBackgroud();
+});
+
+/******************************
+    SCROLL SPY  
+******************************/
+
+window.onload = function () {
+  scrollSpy('#navbar-list', {
+    sectionSelector: '.subjects-section .section-container section',
+    targetSelector: 'a',
+    hrefAttribute: 'href',
+    offset: 90,
+    activeClass: 'active',
+  });
+};
